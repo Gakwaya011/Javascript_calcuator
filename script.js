624a42e15325ca64d57bdc13e23cde4239854491
Christophe Gakwaya <christophegakwaya@gmail.com>
2024-02-06 21:39:36 +0200

    new updated

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function change() {
     var numOne = document.querySelector("#num1").value;
     var numTwo = document.querySelector("#num2").value;
     var numThree = document.querySelector("#num3").value;
-    var bNumOne = parseFloat(31) - parseFloat(numOne)
-    var bNumTwo = parseFloat(12) - parseFloat(numTwo)
-    var bNumThree = parseFloat(2023) - parseFloat(numThree)
+    var bNumOne = parseInt(2) - parseInt(numOne)
+    var bNumTwo = parseInt(2) - parseInt(numTwo)
+    var bNumThree = parseInt(2024) - parseInt(numThree)
+    if (bNumTwo < 0) {
+        bNumTwo = bNumTwo + 12
+        bNumThree = bNumThree - 1
+    }
+
+
     var outPutMessage = "your age is " + bNumThree + " Years " + bNumTwo + " Months " + bNumTwo + " Days"
     document.querySelector("#output").innerHTML = outPutMessage
 
